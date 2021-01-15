46638dac93d4d6bffb8d1f6e621152dd9d19828b
EfeYks534 <yefeyks@gmail.com>
2021-01-16 00:32:59 +0300

    Fix retarded bug

--- src/Memory/MemMng.h
+++ src/Memory/MemMng.h
@@ -12,7 +12,7 @@ void PhysFree(void *addr, size_t size);
 
 void PhysInit(struct stivale_struct *boot_data);
 
-void *MMap(void *vir_addr, void *phys_addr, );
+void *MMap(void *vir_addr, void *phys_addr);
 
 void MemInit(struct stivale_struct *boot_data);
 
